948515a624af779ca8b70b839df7c704429f181c
Liz <lizarbieto@lizs-mbp.home>
2025-03-14 14:50:10 -0400

    updating for prototype

---
+++ HocusFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HocusFocusApp.swift
+//  HocusFocusApp
+//
+//  Created by liz arbieto on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct HocusFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
